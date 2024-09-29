f5ff9b57da9694c80074e98fbaaed421dfb3bacf
Prudhviraj Gonnabhaktula <p.gonnabhaktula@waterstons.com>
2024-09-29 12:22:59 +0100

    test

--- src/app.js
+++ src/app.js
@@ -2,11 +2,17 @@ const express = require("express");
 
 const app = express();
 
+
+
 app.use("/test", (req,res) => {
+    res.send("Hello World test")
+});
+
+app.use("/", (req,res) => {
     res.send("Hello World")
 });
 
-app.listen(3000, ()=>{
-    console.log("Server runninng on port 3000")
+app.listen(4545, ()=>{
+    console.log("Server runninng on port 4545")
 });
 
